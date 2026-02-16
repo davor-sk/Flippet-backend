519b8ca35bc1c8e64406d8173e18f0669d0d202c
Davor <davor.skiljic@gmail.com>
2026-02-16 21:21:07 +0100

    Added MongoDB connection and CORS

---
+++ db.js
@@ -0,0 +1,25 @@
+import { MongoClient } from "mongodb";
+import { config } from "dotenv";
+
+config();
+
+const username = process.env.MONGO_USERNAME;
+const password = process.env.PASSWORD;
+const cluster = process.env.MONGO_CLUSTER;
+
+const mongoURI = `mongodb+srv://${username}:${password}@${cluster.toLowerCase()}.8kmdg0i.mongodb.net/?appName=${cluster}`;
+
+async function connectToDatabase() {
+  try {
+    const client = new MongoClient(mongoURI);
+    await client.connect();
+    console.log("Uspješno spajanje na MongoDB bazu");
+    let db = client.db("users_db");
+    return db;
+  } catch (error) {
+    console.error(`Došlo je do greške pri spajanju na bazu! Greška: ${error}`);
+    throw error;
+  }
+}
+
+export { connectToDatabase };
--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import express from "express";
+import cors from "cors";
+import { connectToDatabase } from "./db.js";
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+app.use(cors());
+
+let db = connectToDatabase();
 
 app.get("/", (req, res) => {
   return res.status(200).json({ message: "Radi!" });
