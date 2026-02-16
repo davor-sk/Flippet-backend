553495205caba3b12ff83fe07fe255e6338d19f0
Davor <davor.skiljic@gmail.com>
2026-02-16 20:07:29 +0100

    Initial commit: setup project and install Express

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  return res.status(200).json({ message: "Radi!" });
+});
+
+app.listen(PORT, (error) => {
+  if (error) {
+    console.error(`Greška prilikom pokretanja poslužitelja: ${error.message}`);
+  } else {
+    console.log(`Server je pokrenut na http://localhost:${PORT}`);
+  }
+});
